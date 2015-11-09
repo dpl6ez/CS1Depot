e947884202d85a70ab0432e24a53ea9f3b48209c
Daniel Lowe <dpl6ez@virginia.edu>
2015-11-09 02:02:52 -0500

    Minor Update

--- python/PythonHw.py
+++ python/PythonHw.py
@@ -4,9 +4,10 @@ n = 10
 
 while (n != 1):
     NumList.append(n)
+    Length = (len(NumList))
     if (n % 2):
         (n // 2)
     else:
         (n * 3) + 1
 else:
-    print str(NumList)
+    print Length
